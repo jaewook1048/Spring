faaa306a1c5da9bc29fc338d07e08d68f25d7351
JaeWookYoon <51107995+jaewook1048@users.noreply.github.com>
2023-10-27 00:32:33 +0900

    AppConfig 수정 (역할과 구현이 직관적으로 보이게, 중복 제거)

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -8,14 +11,25 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
 // 애플리케이션 전체를 구성하고 설정하는 클래스
+// 역할과 구현이  직관적으로 보임
+// 구성 영역
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
 
